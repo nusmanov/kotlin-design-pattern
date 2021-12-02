e8264098f3ce98c6728fae9f54e5b5d095cc7324
nusmanov <nodirbek.usmanov@sevdesk.de>
2021-12-02 08:28:57 +0100

    kotlin playground to implement design patterns to enrich the kotlin exposed query

---
+++ src/main/kotlin/com/example/querysearchdesignpattern/BookRepository.kt
@@ -0,0 +1,33 @@
+package com.example.querysearchdesignpattern
+
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.springframework.stereotype.Repository
+
+@Repository
+class BookRepository {
+
+    fun getAll(): List<BookDto> = transaction { BookTable.selectAll().map { BookDto.fromResultRow(it) } }
+
+    fun create(bookName: String): Int = transaction { BookTable.insertAndGetId { it[name] = bookName }.value }
+
+    fun getBook(id: Int): BookDto {
+        return transaction {
+            BookTable.select { BookTable.id eq id }.limit(1).single().let { BookDto.fromResultRow(it) }
+        }
+    }
+
+    fun fireToDb(): List<BookDto> =
+
+        transaction {
+
+            // search filter + pagination
+            val baseQuery: Query = BookTable.selectAll()
+
+            // do some cool stuff here
+            listOf()
+        }
+}
+
+
