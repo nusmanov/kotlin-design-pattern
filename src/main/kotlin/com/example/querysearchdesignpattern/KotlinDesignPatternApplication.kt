cc38ec10a981aa1b2824b038d68f66973ccfc6a0
nusmanov <nodirbek.usmanov@sevdesk.de>
2021-12-02 08:15:29 +0100

    kotlin playground to implement design patterns to enrich the kotlin exposed query

---
+++ src/main/kotlin/com/example/querysearchdesignpattern/KotlinDesignPatternApplication.kt
@@ -0,0 +1,11 @@
+package com.example.querysearchdesignpattern
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinDesignPatternApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinDesignPatternApplication>(*args)
+}
